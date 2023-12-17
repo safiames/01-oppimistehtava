474756fc7a7e85206da4e3c87076e42ebdcd7d5b
Safia Messaoud <81869764+safiames@users.noreply.github.com>
2023-12-17 10:24:55 +0200

    add pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/safiames/bc-portfolio",
+  base: "https://safiames.github.io",
   plugins: [react()],
 });
